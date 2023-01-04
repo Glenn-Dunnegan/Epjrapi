f2ff83a313475223874428c7fadb6ea36746bcb5
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2023-01-04 04:19:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
   return res.send({errMsg: err.message})
 })
 
-app.listen(3001, () => {
-  console.log(`Server is running on port ${3001}`)
+app.listen(9000, () => {
+  console.log(`Server is running on port ${9000}`)
 })
