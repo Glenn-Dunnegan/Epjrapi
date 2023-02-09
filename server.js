b329695b66e8ab3bbdf95c12e012c131ae220bfd
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2023-02-09 17:12:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -39,15 +39,28 @@ app.use('/api/access', require('./routes/accessRouter.js'))
 app.use('/jobImage', require('./routes/openRouter.js'))
 app.use('/forgotPassword', require('./routes/forgotPasswordRouter.js'))
 
+let connCount = 0
+
+//let socketsArray = await io.fetchSockets();
+
+const getSockets = async function(){
+  const socketsArray = await io.fetchSockets();
+  console.log(socketsArray.length)
+}
+
 io.on('connection', (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  
+  console.log(`Socket ${socket.id}`);
+  connCount++
+  
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    socket.disconnect()
   });
-  socket.on('eventTime_update', (data) => {
-    console.log(data.message)
-    socket.broadcast.emit('updateSchedule', data)
+  socket.on('eventTime_update', () => {
+    socket.broadcast.emit('updateSchedule')
   })
+  getSockets()
 });
 
 app.use((err, req, res, next) => {
