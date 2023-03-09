966b52e8c4947d6e76c999079e2e141fd8af3b4f
Glenn Dunnegan <41714994+Glenn-Dunnegan@users.noreply.github.com>
2023-03-09 08:11:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 const express = require('express')
 const app = express()
-const https = require('https')
+//const https = require('https')
 const fs = require('node:fs');
-//const http = require('http')
+const http = require('http')
 require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
@@ -13,13 +13,13 @@ const cors = require('cors')
 const upload = multer({ dest : 'uploads/'})
 const { Server } = require("socket.io");
 
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/dirtandseptic.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/dirtandseptic.com/fullchain.pem'),
-};
+// const options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/dirtandseptic.com/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/dirtandseptic.com/fullchain.pem'),
+// };
 
-const server = https.createServer(options, app);
-// const server = http.createServer(app);
+//const server = https.createServer(options, app);
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
