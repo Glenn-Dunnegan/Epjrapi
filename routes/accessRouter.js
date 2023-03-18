88a21f0eb533cddd93b9358ae5d31aabf31196d6
Glenn Dunnegan <41714994+Glenn-Dunnegan@users.noreply.github.com>
2023-03-18 14:28:53 -0400

    Update accessRouter.js

--- routes/accessRouter.js
+++ routes/accessRouter.js
@@ -583,6 +583,14 @@ accessRouter.get('/work/search/:searchType/:searchParam', (req, res, next) => {
                     }
                     return res.status(200).send(jobs)
                 })
+            }else if(type === '_id'){
+                Job.find({[type]: req.params.searchParam},(err, jobs) => {
+                    if(err){
+                      res.status(500)
+                      return next(err)
+                    }
+                    return res.status(200).send(jobs)
+                })
             }else{
                 Job.find({[type]: {$regex:  req.params.searchParam, '$options': 'i'}},(err, jobs) => {
                     if(err){
