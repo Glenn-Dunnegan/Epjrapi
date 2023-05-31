2747b10fae40e70da136fcb4d3b2ac8664af0f70
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2023-05-31 09:11:32 -0500

    adding LOM

---
+++ models/lom.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+mongoose.set('sanitizeProjection', true)
+
+const matSchema = new Schema({
+    material: {
+        type: String,
+        required: true
+    },
+    matCount: {
+        type: Number
+    },
+    matUnit: {
+        type: String
+    }
+  })
+
+const lomSchema = new Schema({
+    
+    list:{
+        type: [matSchema]
+    },
+    dateChanged:{
+        type: Date,
+        default: Date.now
+    },
+    forJob:{
+        type: Schema.Types.ObjectId,
+        ref: "Job",
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Lom", lomSchema)
